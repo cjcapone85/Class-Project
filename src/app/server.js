fb80ba7016cd4097db5483110bb237c0cc79c5d5
cjcapone85 <99698474+cjcapone85@users.noreply.github.com>
2022-05-09 19:18:10 -0500

    Update server.js

---
+++ src/app/server.js
@@ -0,0 +1,10 @@
+const exp = require("constants");
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("./dist/Class-Project/"));
+
+app.get("/*", (req, res) => res.sendFile("inde.heml", { root: "dist/Class-Project"}));
+
+app.listen(process.env.PORT || 8080);
