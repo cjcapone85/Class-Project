1a2921d0c60dcc7c99412366e3011a24c5806ed6
cjcapone85 <99698474+cjcapone85@users.noreply.github.com>
2022-05-09 19:51:55 -0500

    app upload

--- src/app/server.js
+++ src/app/server.js
@@ -2,8 +2,8 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.static("./dist/Class-Project/"));
+app.use(express.static("./dist/Class-Project"));
 
-app.get("/*", (req, res) => res.sendFile("inde.heml", { root: "dist/Class-Project"}));
+app.get("/*", (_, res) => res.sendFile("index.html", { root: "dist/Class-Project/"}));
 
 app.listen(process.env.PORT || 8080);
