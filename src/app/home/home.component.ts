649c26d7ad9941d9e65d35762d11563e673ca6d0
cjcapone85 <99698474+cjcapone85@users.noreply.github.com>
2022-06-30 17:00:00 -0500

    App Cleanup
    deleted unused code and spacing to make the code look cleaner

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 import { SearchService } from '../search.service';
 import { delay, map, Observable } from 'rxjs';
 import { Movie } from '../searchResults.model';
-// import { SearchResults } from '../searchResults.model'
 
 
 @Component({
@@ -22,14 +21,12 @@ export class HomeComponent implements OnInit {
   query:string =`query=${""}`;
 
   topMoviesUrl="https://api.themoviedb.org/3/trending/all/week?api_key=74c8fbfdf29b471999f8a5ee6ec15a43"
-  // searchUrl= this.base_URL+this.multiSearch+this.apiKey+this.query;
-  searchUrl=`https://api.themoviedb.org/3/search/multi?api_key=74c8fbfdf29b471999f8a5ee6ec15a43&language=en-US&query=${'batman'}&page=1&include_adult=false`
+  // searchUrl=`https://api.themoviedb.org/3/search/multi?api_key=74c8fbfdf29b471999f8a5ee6ec15a43&language=en-US&query=${'batman'}&page=1&include_adult=false`
   genreUrl="https://api.themoviedb.org/3/genre/movie/list?api_key=74c8fbfdf29b471999f8a5ee6ec15a43&language=en-US"
   results: any;
 
 
   main = document.getElementById('main');
-  // results:object = this.http.get(this.searchUrl)
 
 
   constructor(private http: HttpClient, private searchResults: SearchService) {
@@ -61,47 +58,4 @@ export class HomeComponent implements OnInit {
     window.location.href=`${this.Img_URL}+${this.results.poster_path}`;
   }
 
-  // getColor(vote_average) {
-  //   if(vote_average >= 8){
-  //     return 'green'
-  //   }else if(vote_average >= 5){
-  //     return 'orange'
-  //   }else{
-  //     return 'red'
-  //   }
-  // }
-
-}
-
-
-  //  search() {
-  //   return this.http.get(`https://api.themoviedb.org/3/search/multi?api_key=${this.apiKey}&language=en-US&query=${'name'}&page=1&include_adult=false`)
-  // }
-
-  // onSubmit() {
-  //   return this.http.get(this.searchUrl).subscribe
-  //   // console.log(this.onSubmit,'submit');
-  // }
-
-  // getColor(vote) {
-  //   if(vote >= 8){
-  //     return 'green'
-  //   }else if(vote >= 5){
-  //     return 'orange'
-  //   }else{
-  //     return 'red'
-  //   }
-  // }
-
-//  }
-
-
-
-// onFetchMovies(searchInput: string) {
-//   this.http
-//   .get('https://api.themoviedb.org/3/genre/movie/list?api_key=74c8fbfdf29b471999f8a5ee6ec15a43&language=en-US')
-//     .subscribe(posts => {
-//       console.log(posts);
-//     });
-
-
+ }
